3a520c597b6d0477b34b4253091b142ea7efeef8
wiltendo <129499132+wiltendo@users.noreply.github.com>
2026-04-01 16:40:49 +0200

    Site V2 (outils)

--- application/public/valid_password.js
+++ application/public/valid_password.js
@@ -1,21 +1,27 @@
 const valider = document.getElementById('submit');
+if (document.getElementById('mdp')!=null){
+    document.getElementById('mdp').addEventListener('input', () => {valid("mdp",8,[true,true,true,true,true])});
+    document.getElementById('mdp').addEventListener("mouseover", function(event) {changeDisplay(event,"Valid_mdp")});
+    document.getElementById('mdp').addEventListener("mouseout", function(event) {changeDisplay(event,"Valid_mdp")});
+}
 
-document.getElementById('mdp').addEventListener('input', () => {valid("mdp",[true,true,true,true,true])});
-document.getElementById('mdp').addEventListener("mouseover", function(event) {changeDisplay(event,"Valid_mdp")});
-document.getElementById('mdp').addEventListener("mouseout", function(event) {changeDisplay(event,"Valid_mdp")});
+if (document.getElementById('name')!=null){
+    document.getElementById('name').addEventListener('input', () => {valid("name",5,[true,true,true,false,false])});
+    document.getElementById('name').addEventListener("mouseover", function(event) {changeDisplay(event,"Valid_name")});
+    document.getElementById('name').addEventListener("mouseout", function(event) {changeDisplay(event,"Valid_name")});
+}
 
-document.getElementById('name').addEventListener('input', () => {valid("name",[true,true,true,false,false])});
-document.getElementById('name').addEventListener("mouseover", function(event) {changeDisplay(event,"Valid_name")});
-document.getElementById('name').addEventListener("mouseout", function(event) {changeDisplay(event,"Valid_name")});
+if (document.getElementById('Prénom')!=null){
+    document.getElementById('Prénom').addEventListener('input', () => {valid("Prénom",3,[true,true,true,false,false])});
+    document.getElementById('Prénom').addEventListener("mouseover", function(event) {changeDisplay(event,"Valid_Prénom")});
+    document.getElementById('Prénom').addEventListener("mouseout", function(event) {changeDisplay(event,"Valid_Prénom")});
+}
 
-document.getElementById('Prénom').addEventListener('input', () => {valid("Prénom",[true,true,true,false,false])});
-document.getElementById('Prénom').addEventListener("mouseover", function(event) {changeDisplay(event,"Valid_Prénom")});
-document.getElementById('Prénom').addEventListener("mouseout", function(event) {changeDisplay(event,"Valid_Prénom")});
+const Valider = {};
 
-
-function valid(element,condition){
+function valid(element,longueur,condition){
     const mdp = document.getElementById(element).value;
-    const lengthCritere = mdp.length >= 8;
+    const lengthCritere = mdp.length >= longueur;
     const majusculeCritere = /[A-Z]/.test(mdp);
     const minusculeCritere = /[a-z]/.test(mdp);
     const chiffreCritere = /[0-9]/.test(mdp);
@@ -28,11 +34,23 @@ function valid(element,condition){
     updateCritere('chiffre_'+element, chiffreCritere == condition[3]);
     updateCritere('special_'+element, specialCritere == condition[4]);
 
+    if (Valider[element] == null){
+        Valider[element] = false;
+    }
+
     if (lengthCritere == condition[0] && 
         majusculeCritere == condition[1] &&        
         minusculeCritere == condition[2] && 
         chiffreCritere == condition[3] && 
         specialCritere == condition[4]){
+        Valider[element] = true;
+    } else {
+        Valider[element] = false;
+    }
+
+    console.log(Valider)
+    console.log(Object.values(Valider).every(value => value === true))
+    if(Object.values(Valider).every(value => value === true)){
         valider.disabled=false;
     } else {
         valider.disabled=true;
