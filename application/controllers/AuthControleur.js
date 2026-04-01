fc1f7350a6d6ea2d6217602e1546f8c29e2979cc
wiltendo <129499132+wiltendo@users.noreply.github.com>
2026-04-01 16:36:27 +0200

    Site V2 (Securiter)

--- application/controllers/AuthControleur.js
+++ application/controllers/AuthControleur.js
@@ -1,42 +1,44 @@
 const User = require('../model/dataUser');
-
+const bcrypt = require('bcrypt');
 
 exports.postAuth = async (req,res,next) => {
     res.locals.log = req.session.isLog;
     console.log('middleware Authentification', req.method);
-    console.log(req.body.Mail);
-    console.log(req.body.password);
 
-
-    
-    console.log(await User.find({Mail: req.body.Mail,Mdp:req.body.password}))
-    console.log(await User.countDocuments({Mail: req.body.Mail,Mdp:req.body.password}))
-    console.log(await User.countDocuments({Mail: req.body.Mail,Mdp:req.body.password}) == 1)
-    if (await User.countDocuments({Mail: req.body.Mail,Mdp:req.body.password}) == 1){
-        req.session.isLog=true;
-        const user = await User.findOne({Mail: req.body.Mail,Mdp:req.body.password},{Role:1,_id:1})
-        req.session.role = user.Role;
-        req.session.user = user._id;
-        
-        res.redirect('/');
+   
+    const user = await User.findOne({Mail: req.body.Mail});
+    if (await User.countDocuments({Mail: req.body.Mail}) == 1){
+        if (await bcrypt.compare(req.body.password,user.Mdp)){
+            req.session.isLog=true;
+            const user = await User.findOne({Mail: req.body.Mail},{Role:1,_id:1})
+            req.session.role = user.Role;
+            req.session.user = user._id;
+            
+            res.redirect('/');
+        }else{
+            res.redirect('/Auth?Erreur="Mot de passe Incorrect"')
+        }
     }else{
-        res.redirect('/Auth')
+        res.redirect('/Auth?Erreur="Mail Incorrect"')
     }
 }
 
 exports.postInscription= async (req,res,next) => {
     console.log('middleware Inscription', req.method);
 
-    req.session.isLog=true;
-    console.log('log session',req.session.isLog);
-    
+    if(res.locals.Département.includes(req.body.Département)){
+        req.session.isLog=true;
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        
+        const user = new User({Nom: req.body.name,Prénom: req.body.Prénom, Mail:req.body.Mail ,Mdp: hashedPassword, Département : req.body.Département, Role : "Utilisateur"});
+        user.save()
+            .then((result) => { console.log("ok")})
+            .catch((err) => {res.redirect('/Inscription?Erreur='+err)});
 
-    const user = new User({Nom: req.body.name,Prénom: req.body.Prénom, Mail:req.body.Mail ,Mdp: req.body.password, Département : req.body.Département, Role : "Utilisateur"});
-    user.save()
-        .then((result) => { console.log("ok")})
-        .catch((err) => {console.log(err)});
-
-    req.session.role = "Utilisateur";
-    req.session.user = await User.countDocuments();
-    res.redirect('/');
+        req.session.role = "Utilisateur";
+        req.session.user = user._id;
+        res.redirect('/');
+    }else{
+        res.redirect('/Inscription?Erreur="Département Inconnu"')
+    }
 }
