504c6ab1da4634cde0f0e139709c742f284de299
wiltendo <129499132+wiltendo@users.noreply.github.com>
2026-05-13 14:07:55 +0200

    Fix admin role check condition in middleware

--- application/auth_middleware/adminroutes.js
+++ application/auth_middleware/adminroutes.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    if (!req.session.role != "Administrateur") {
+    if (req.session.role != "Administrateur") {
         return res.redirect('/');
     }
     next();
